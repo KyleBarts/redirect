665c455605cfe4937c1e6112ff56630a2fd91424
Kyle Bartido <kpibartido@gmail.com>
2025-12-31 11:35:07 +0800

    Text change

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,15 @@ export default function Home() {
     <main className="page">
       <section className="content">
         <p className="headline">
-          This is Barts personal link redirecting tool.
+          This is Barts' personal link redirecting tool.
         </p>
 
         <p className="subtext">
           Down with big URL.
+        </p>
+
+        <p className="muted">
+          Why are you even here. You were supposed to follow the link man
         </p>
       </section>
     </main>
